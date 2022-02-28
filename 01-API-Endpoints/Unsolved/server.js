2badb0d482c3afbeb04b770c374c21aad25b55ae
normadiaz <normadiazchb@me.com>
2022-02-28 11:07:18 -0600

    solved

--- 01-API-Endpoints/Unsolved/server.js
+++ 01-API-Endpoints/Unsolved/server.js
@@ -1,18 +1,25 @@
 // TODO: Import express
-
+const express = require('express');
 // TODO: Import 'terms.json' file
+const terms = require('./terms.json');
 const PORT = 3001;
 
 // TODO: Initialize app variable
+const app = express()
 
 // Add a static route for index.html
 app.get('/html', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
   // `res.sendFile` is Express' way of sending a file
   // `__dirname` is a variable that always returns the directory that your server is running in
-  res.sendFile(__dirname + '/index.html');
+  // res.sendFile(__dirname + '/index.html');
 });
 
 // TODO: Create a route for a GET request that will return the content of our `termData.json` file
+
+app.get('/data', (req, res) => {
+  res.send(terms)
+})
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
